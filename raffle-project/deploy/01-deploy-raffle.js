2ec5714f55333aa351064e3bf16d9fdc399989b0
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2023-02-09 16:56:24 +0200

    updated some creds

--- raffle-project/deploy/01-deploy-raffle.js
+++ raffle-project/deploy/01-deploy-raffle.js
@@ -1,4 +1,6 @@
-module.exports = async function({ getNamedAccounts, deployemnts }) {
-    const { deploy, log } = deployemnts
-    const { deployer } = getNamedAccounts
-}
+module.exports = async function ({ getNamedAccounts, deployemnts }) {
+  const { deploy, log } = deployemnts;
+  const { deployer } = await getNamedAccounts;
+
+  
+};
--- raffle-project/hardhat.config.js
+++ raffle-project/hardhat.config.js
@@ -7,8 +7,9 @@ require('hardhat-contract-sizer');
 require('dotenv').config();
 
 
-const RINKEBY_RPC_URL  = process.env.RINKEBY_RPC_URL
-
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: 'hardhat',
